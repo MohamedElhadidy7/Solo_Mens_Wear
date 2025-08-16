4b5f15bcd21ac2019cd58725ebc01fa97ce81970
mohamed elhadidy <melhadidy43@gmail.com>
2025-08-16 15:02:38 +0300

    create forget password model

---
+++ lib/Features/Auth/data/Models/Forget_Password_Model/ForgetPassword_Model.dart
@@ -0,0 +1,21 @@
+class ForgetpasswordModel {
+  final String status;
+  final String message;
+  final bool isSuccess;
+  ForgetpasswordModel({
+    required this.status,
+    required this.message,
+    required this.isSuccess,
+  });
+
+  factory ForgetpasswordModel.fromjson(Map<String, dynamic> json) {
+    return ForgetpasswordModel(
+      status: json['status'],
+      message: json['message'],
+      isSuccess: json['isSuccess'],
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {"status": status, "message": message, "isSuccess": isSuccess};
+  }
+}
